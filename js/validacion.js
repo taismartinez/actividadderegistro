a779b340935416b450991549767aa54b5e0fe408
taismartinez <tati06mar@gmail.com>
2023-08-09 20:57:49 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -6,27 +6,36 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-init();
+document.addEventListener("DOMContentLoaded", function () {
+    const miCheckbox = document.getElementById('terminos');
+    const botonComprobar = document.getElementById('regBtn');
+    botonComprobar.addEventListener("click", function() {
+      if (miCheckbox.checked) {
+        showAlertSuccess();
+        
+      } else {
+       showAlertError();
+      }
+    });
+});
 
-function init(){
 
-    document.getElementById("regBtn").addEventListener("click", validarRegistro)
-}
-
+document.getElementById("regBtn").addEventListener("click", validarRegistro)
 
 function validarRegistro(){
-
     let password1 = document.getElementById("password1").value
     let password2 = document.getElementById("password2").value
-    
-    if (password1 != password2) {
+    let name = document.getElementById("nombre").value
+    let lastname = document.getElementById("apellido").value
+    let email = document.getElementById("email").value
+    let btn = document.getElementById("terminos").value
+    if (password1 !== password2) {
         showAlertError()
-    } else if (password1.length < 6){
+    }else if (password1.length < 6){
         showAlertError()
-    }
-    else{
-    
+    }else if (!name || !lastname || !email || !btn){
+        showAlertError()
+    }else{
         showAlertSuccess();
-    }
-    
-}
+        }  
+} 
